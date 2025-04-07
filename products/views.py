ed908882904435a04f01961dea05fe33beba212a
shevchenko-nikita <nicksevchenkos@gmail.com>
2025-04-07 23:56:11 +0300

    working on products page

--- products/views.py
+++ products/views.py
@@ -4,4 +4,10 @@ def index(request):
     return render(request, 'products/index.html')
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'image_url': 'static/images/book1.jpg',
+        'title': 'first book',
+        'description': 'test text for description la la la',
+        'price': 200,
+    }
+    return render(request, 'products/products.html', context)
